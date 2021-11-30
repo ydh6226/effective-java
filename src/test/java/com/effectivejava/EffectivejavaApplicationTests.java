df5fc695307faa373906d71cb38602c065bca697
ydh6226 <ydh6226@naver.com>
2021-11-30 23:13:28 +0900

    이펙티브자바 프로젝트 생성

---
+++ src/test/java/com/effectivejava/EffectivejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effectivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectivejavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
